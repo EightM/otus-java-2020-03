6c77d40636f893c38e97a4f857c20a775da66736
Viktor Gukov <zchokobo@gmail.com>
2020-03-31 12:43:03 +0300

    Изменение работы с зависимостями

--- buildSrc/src/main/java/Dependencies.java
+++
@@ -1,4 +0,0 @@
-public interface Dependencies {
-    String guava = String.format("com.google.guava:guava:%s", Versions.guava);
-    String junit = String.format("junit:junit:%s", Versions.junit);
-}
