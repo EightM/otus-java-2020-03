9a1559328fc142e3f7b8bc16647e70b36643364d
Viktor Gukov <zchokobo@gmail.com>
2020-03-31 09:03:50 +0300

    1. Добавлен толстый jar 2. Добавлен apply false для родительского build.gradle 3. Работа с зависимостями и версиями вынесена в buildSrc

---
+++ buildSrc/src/main/java/Dependencies.java
@@ -0,0 +1,4 @@
+public interface Dependencies {
+    String guava = String.format("com.google.guava:guava:%s", Versions.guava);
+    String junit = String.format("junit:junit:%s", Versions.junit);
+}
---
+++ buildSrc/src/main/java/Versions.java
@@ -0,0 +1,4 @@
+public interface Versions {
+    String guava = "28.2-jre";
+    String junit = "4.12";
+}
