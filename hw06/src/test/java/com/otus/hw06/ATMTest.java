01449f4e2490dfea0bc6aa6b96f494b4fb8c9214
Viktor Gukov <zchokobo@gmail.com>
2020-05-15 14:29:39 +0300

    Обновление тестов

--- hw06/src/test/java/com/otus/hw06/ATMTest.java
+++ hw06/src/test/java/com/otus/hw06/ATMTest.java
@@ -9,11 +9,10 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ATMTest {
-    private static final Map<Banknotes, BanknotesCell> initMoney = new EnumMap<>(Banknotes.class);
+    private final Map<Banknotes, BanknotesCell> initMoney = new EnumMap<>(Banknotes.class);
 
     @BeforeEach
     void initMoney() {
-        initMoney.clear();
         initMoney.put(Banknotes.FIVE_THOUSAND, new BanknotesCell(0));
         initMoney.put(Banknotes.THOUSAND, new BanknotesCell(1));
         initMoney.put(Banknotes.FIVE_HUNDRED, new BanknotesCell(0));
@@ -38,6 +37,17 @@ public class ATMTest {
         ATM testAtm = new ATM(initMoney);
         testAtm.getMoney(1500);
         assertEquals(0, testAtm.getBalance());
+    }
+
+    @Test
+    public void putMoney() {
+        ATM testAtm = new ATM(initMoney);
+        Map<Banknotes, BanknotesCell> newMoney = new EnumMap<>(Banknotes.class);
+        newMoney.put(Banknotes.FIVE_THOUSAND, new BanknotesCell(3));
+        newMoney.put(Banknotes.THOUSAND, new BanknotesCell(4));
+        newMoney.put(Banknotes.FIVE_HUNDRED, new BanknotesCell(1));
+        testAtm.putMoney(newMoney);
+        assertEquals(21000, testAtm.getBalance());
     }
 
     @Test
