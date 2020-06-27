d7eaf3258051b38584f22c0e1eed7c63daa7c196
Viktor Gukov <zchokobo@gmail.com>
2020-06-27 12:08:22 +0300

    Перенос HW11 в HW12

---
+++ hw12/src/main/java/ru/otus/core/model/User.java
@@ -0,0 +1,92 @@
+package ru.otus.core.model;
+
+
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(targetEntity = ru.otus.core.model.AddressDataSet.class, fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id")
+    private AddressDataSet address;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "user")
+    private Set<PhoneDataSet> phones;
+
+    public User() {
+    }
+
+    public User(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public AddressDataSet getAddress() {
+        return address;
+    }
+
+    public void setAddress(AddressDataSet address) {
+        this.address = address;
+    }
+
+    public Set<PhoneDataSet> getPhones() {
+        return phones;
+    }
+
+    public void setPhones(Set<PhoneDataSet> phones) {
+        this.phones = phones;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address=" + address +
+                ", phones=" + phones +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(address, user.address) &&
+                Objects.equals(phones, user.phones);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, address, phones);
+    }
+}
